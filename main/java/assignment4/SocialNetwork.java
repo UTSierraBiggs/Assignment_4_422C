c03c511aba69d0b1c6b2315d91dd24f56f8645c1
Sierra Biggs <sierrabiggs@utexas.edu>
2017-11-20 18:59:26 -0600

    first structures of clique finding method

--- main/java/assignment4/SocialNetwork.java
+++ main/java/assignment4/SocialNetwork.java
@@ -1,8 +1,6 @@
 package assignment4;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Social Network consists of methods that filter users matching a
@@ -45,8 +43,63 @@ public class SocialNetwork {
      * @return list of set of all cliques in the graph
      */
     public static List<Set<String>> findCliques(List<assignment4.Tweets> tweets) {
+        //Create a hashmap and load all the users that have made tweets as keys
+        HashMap<String, ArrayList<String>> whoTagsWho = new HashMap();
+        boolean keyListComparison = false;
+        for(int j = 0 ; j < tweets.size(); j ++){
+            if(whoTagsWho.get(tweets.get(j).getName()) == null){
+                whoTagsWho.put(tweets.get(j).getName(), new ArrayList<String>());
+            }
+        }
+
+        // for each user, a, (as keys) in the hashmap, compare it to every user tagged in each
+        // tweet and if they ARE tagged in that tweet, made by user b, add b
+        // to the String arraylist of user a in the hashmap
+        for (Map.Entry<String, ArrayList<String>> entry : whoTagsWho.entrySet()) {
+            for (int i = 0 ; i < tweets.size() ; i ++ ){
+                String[] splitTweetText= tweets.get(i).getText().split(" ");
+                for(int k = 0 ; k < splitTweetText.length; k ++){
+                    String filteredString = splitTweetText[k].replaceAll("[^A-Za-z0-9@]", "");
+                    if(entry.getKey().equals(filteredString)){
+                        ArrayList<String> temp = whoTagsWho.get(entry.getKey());
+                        temp.add(filteredString);
+                        whoTagsWho.put(entry.getKey(), temp);
+                    }
+                }
+            }
+        }
+
+        for (Map.Entry<String, ArrayList<String>> entry : whoTagsWho.entrySet()) {
+            for (int i = 0 ; i < entry.getValue().size(); i ++){
+                if (whoTagsWho.get(entry.getValue().get(i)) != null) {
+                    for (int j = 0; j < whoTagsWho.get(entry.getValue().get(i)).size(); j++){
+                        keyListComparison = true;
+                    }
+                }
+            }
+            for(int k = 0 ; k < entry.getValue().size(); k ++){
+                for(int l = k+1; l < entry.getValue().size(); l++ ){
+                    if (whoTagsWho.get(entry.getValue().get(l)) != null) {
+
+                    }
+                }
+            }
+        }
+
+
+
+
         List<Set<String>> result = new ArrayList<Set<String>>();
         return result;
+    }
+
+    public boolean Contains(String key, ArrayList<String> valueList){
+        for (int i = 0 ; i < valueList.size(); i ++){
+            if (valueList.get(i).equals(key)){
+                return true;
+            }
+        }
+        return false;
     }
 }
 
